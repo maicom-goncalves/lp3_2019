74b167e3052bdcc4406d5e31a953377166dce3b5
MaiconMoreira <maiconmoreirams@hotmail.com>
2019-11-29 22:19:09 -0400

    trabalho blog-api

---
+++ blog-api/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const rotasArtigos = require('./routes/artigos');
+require('./databases/db');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/artigos', rotasArtigos);
+
+
+module.exports = app;
